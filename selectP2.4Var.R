53b542c87c80313c49e4c3d30b824ebd8819e2d2
dingmindm <dingmin.dm@gmail.com>
2013-10-07 17:52:52 +1100

    single file

---
+++ selectP2.4Var.R
@@ -0,0 +1,19 @@
+# this Script is used to select useful attributes from POAMA
+
+
+require("ncdf4")
+
+rm(list = ls(all = TRUE))
+
+# set file root
+root <- "E:/climateData/POAMA2.4/A/"
+
+fList <- list.files(root,recursive = TRUE)
+
+##À©Õ¹
+i <- 1
+filename <- paste0(root,fList[i])
+
+var <- 'hr24_prcp'
+ncid <- nc_open(filename,write=FALSE)
+hr24_prcp <- ncvar_get(ncid,var)
