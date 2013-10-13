5a388214447a63b6597bc2422dfe167bb58f3a22
dingmindm <dingmin.dm@gmail.com>
2013-10-14 00:04:57 +1100

    select POAMA and sites data

---
+++ SelectStation.R
@@ -0,0 +1,31 @@
+rm(list=ls())
+# load point data
+load("ref.station.RData")
+names(ref.station)
+station <- ref.station$sites
+# compare Lat Lon, select usefull station
+
+sNum<-length(station$Lat)
+selStation <- data.frame()
+# (-33.51634)>=Lat>=(-38.48167)
+# (142.5)<=Lon<=(147.5)
+
+for(i in 1:sNum){
+  if(station$Lat[i]<=-33.51634 && station$Lat[i]>= -38.48167 && station$Lon[i]>=142.5 && station$Lon[i]<=147.5)
+    selStation <- rbind(selStation,station[i,])
+}
+
+# select raindall from useful sites
+monthlyAverageRainfall<-ref.station$monthlyAverageRainfall
+a<-c(c(1,2),c(as.numeric(row.names(selStation))+2))
+monthlyAverageRainfall<-subset(monthlyAverageRainfall, select = a)
+# select date
+rainFall<-data.frame()
+L<-length(monthlyAverageRainfall[,1])
+
+for (i in c(1:L)){
+  if (monthlyAverageRainfall[i,2]>=1981)
+    rainFall<-rbind(rainFall,monthlyAverageRainfall[i,])
+}
+
+save(rainFall,selStation,file="siteSel.Rdata")
