dce9267b1af510d8dd9896a6b6d90ceb022eedc5
dingmindm <dingmin.dm@gmail.com>
2013-10-18 00:01:51 +1100

    更新

--- ShowNcFile.R
+++ ShowNcFile.R
@@ -1,8 +1,18 @@
 require("ncdf4")
 rm(list = ls(all = TRUE))
 
-filename ="E:/IORFm_201012.nc" #"Indian Ocean Dipole", "El Nino Modoki Index
+#filename ="E:/IORFm_201012.nc" #"Indian Ocean Dipole", "El Nino Modoki Index
+
 #filename = "E:/climateData/POAMA2.4/IOF_1960_2011.nc"
+#filename <- "E:/climateData/POAMA2.4/A/mac_19810201.nc"
+filename <- "C:/Users/DingMin/Downloads/IOA_1960_2011.nc"
+
+#filename = "C:/Users/DingMin/Downloads/IORFm_201310.nc"
+#filename = "C:/Users/DingMin/Downloads/mo_201310.nc"
+#filename = "E:/TDDOWNLOAD/so_201109.nc"
+#filename = "E:/TDDOWNLOAD/mo_201109.nc"
+#filename = "E:/TDDOWNLOAD/mo_clim12_1981-2010.nc"
+#filename = "E:/TDDOWNLOAD/heatflux_input_2011.nc"
 
 # show the dimension information of selected NC file
 nc <- nc_open( filename )
@@ -18,6 +28,9 @@ for( i in 1:nc$ndims ) {
     print(paste(" Unlimited:",d$unlim))
   }
 print(nc)
-tVar<-ncvar_get(nc,"region_index")
+tVar<-ncvar_get(nc,"nino3")
+tVar[1]
+#tVar<-ncvar_get(nc,"sfc_temp")
+#tVar<-ncvar_get(nc,"region_index")
 #tVar<-ncvar_get(nc,"region_name")
 #nc<-nc_close(nc)
